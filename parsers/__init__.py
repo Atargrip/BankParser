14ec6bdb8c56e31b28c56131a595fb21ce492f02
Osasuwu <petrkudr2@gmail.com>
2026-03-05 18:30:51 +0500

    Добавлен Bank RBK parser и внедрён в processor

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,4 +1,5 @@
 from .otbasy import OtbasyParser
 from .halyk import HalykParser
+from .rbk import RBKParser
 
-__all__ = ['OtbasyParser', 'HalykParser']
+__all__ = ['OtbasyParser', 'HalykParser', 'RBKParser']
